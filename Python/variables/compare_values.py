b77945d7f122e78986ad460941b538621271896a
K0ndrich <123kondrich@gmail.com>
2024-07-20 18:44:22 +0300

    edited

---
+++ Python/variables/compare_values.py
@@ -0,0 +1,34 @@
+# сравнения чисел
+x = 10
+y = 9
+
+# bool -> 1 , -1 , 3.2 , True , "Text" , [1,2] , {"key": "yes"}       -> True (правдивое значение)
+# bool -> 0 , 0.0 , False , "" , [] , {} , None , set() , range(0,0)  -> False (ложное значение)
+# функция конвертирует указаное значение в логическое
+bool()
+
+# == равно
+x == y
+# != не равно
+x != y
+# > больше
+x > y
+# < больше
+x < y
+# >= больше или равно
+x >= y
+# <= меньше или равно
+x <= y
+
+
+# not ==>
+# not True  -> False
+# not False -> True
+not False
+
+
+# and ==>
+# True  + True  -> True
+# False + True  -> False
+# False + False -> False
+x > 5 and y < 2
