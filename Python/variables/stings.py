535c8d3c64685000f9cbf25ae33bc6dcfe822362
K0ndrich <123kondrich@gmail.com>
2024-07-26 11:10:31 +0300

    edited

--- Python/variables/stings.py
+++ Python/variables/stings.py
@@ -14,3 +14,30 @@ full_name = first_name + " " + last_name
 # len функция возвращает длину строки
 lenght_string = len(my_string1)
 
+my_first_string = "Max Kondrich"
+my_last_string = "Max"
+# in позволяет проверить являеться ли вторая строка подстрокой для первой, регист емеет значение
+"Max" in my_first_string
+my_last_string in my_first_string
+
+
+# -----   Методы Строк   -------------------------------------------------------------------------------------------------------------------------------
+my_string = "text"
+
+# .upper() метод возвращает значение строки в верхнем регистре
+my_string.upper()
+
+# .lower() метод возвращает значение строки в нижнем регистре
+my_string.lower()
+
+# .strip() убирате пробели до и после текста, тем самым уменьшает длину строки
+my_string = "    text    ---  "
+my_string.strip()
+
+# .replace("old_text" , "new_text") изменяет указаную часть текста на новую
+my_string = "hello world"
+my_string.replace("world", "max")
+
+# .count() возвращет количество указаных символом в строке
+my_string = "text text text"
+my_string.count("t")
