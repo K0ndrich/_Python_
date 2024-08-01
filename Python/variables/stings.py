91d3b107661e8f157b9c005d5a37ce4bbc228926
K0ndrich <123kondrich@gmail.com>
2024-08-01 11:21:49 +0300

    edited

--- Python/variables/stings.py
+++ Python/variables/stings.py
@@ -1,24 +1,42 @@
 # простая строка
 my_string1 = "Hello World"
 
+
 # строка содержит большое количество текста, отступы при етом сохраняютсья
 my_string2 = """Text Text Text 
 Text Text Text Text Text Text
                     Text Text Text"""
+
 
 first_name = "Max"
 last_name = "Kondrich"
 # конкатонация строк, обьединение строк в одну
 full_name = first_name + " " + last_name
 
+
 # len функция возвращает длину строки
 lenght_string = len(my_string1)
+
 
 my_first_string = "Max Kondrich"
 my_last_string = "Max"
 # in позволяет проверить являеться ли вторая строка подстрокой для первой, регист емеет значение
 "Max" in my_first_string
 my_last_string in my_first_string
+
+
+# f-строки , внутри их можно вставлять переменные
+name = "Kondrich"
+age = 25
+number = 10
+f"-- hello {name} -> {age} --"  # -> -- hello Kondrich -> 25 --
+f"-- {age*number} --"  # -> -- 250 --
+
+
+# .format позволяет вставлять внутрь строки оперделенные указаные значения
+my_string = "hello {my_value}".format(my_value="123")  # -> "hello 123"
+my_string = "hello {0} {1}".format("max", "kondrich")  # -> "hello max kondrich"
+my_string = "hello {:%}".format(123.3)  # -> "hello 12330.000000%"
 
 
 # -----   Методы Строк   -------------------------------------------------------------------------------------------------------------------------------
@@ -36,8 +54,16 @@ my_string.strip()
 
 # .replace("old_text" , "new_text") изменяет указаную часть текста на новую
 my_string = "hello world"
-my_string.replace("world", "max")
+my_string.replace("world", "max")  # -> "hello max"
 
 # .count() возвращет количество указаных символом в строке
 my_string = "text text text"
 my_string.count("t")
+
+# .isdigit() возвращает True если строка состоит полность из цифр (нету букв)
+my_string = "123"
+my_string.isdigit()
+
+# .isalpha() возвращает True если строка состоит полностью из букв (нету цифр)
+my_string = "abc"
+my_string.isalpha()
