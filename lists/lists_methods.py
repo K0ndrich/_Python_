cf103a4644cab37ae3017a92b326bec22fd9082c
K0ndrich <123kondrich@gmail.com>
2024-08-12 14:29:50 +0300

    edited

---
+++ lists/lists_methods.py
@@ -0,0 +1,15 @@
+# Методы Списков
+
+
+# .append(my_value) добавляет указаное значение в конец списка
+my_list = ["apple", "bananas", "cherry"]
+my_list.append("orange")  # -> ['apple', 'bananas', 'cherry', 'orange']
+
+# .extend(my_list2) добавялет указаный список к первому списку в конец
+my_list1 = [1, 2, 3]
+my_list2 = [3, 4, 5]
+my_list1.extend(my_list2)
+
+# .insert(index , my_value) вставляем значение в список по индексу (добавление елемента, а не изменение существующего)
+my_list = [1, 2, 3]
+my_list.insert(2, 4)  # -> [1, 2, 4, 3]
