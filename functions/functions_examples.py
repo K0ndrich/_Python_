400bf391f9b9744bfda050a735bece0e9ea45aa4
K0ndrich <123kondrich@gmail.com>
2024-08-15 15:26:13 +0300

    edited

--- functions/functions_examples.py
+++ functions/functions_examples.py
@@ -1,13 +1,34 @@
-# Примеры использования функций
+# Примеры Использования Функций
 
+# 1) Пример
 my_numbers1 = [3, 7, 4, 1, 7, 8, 2]
 my_numbers2 = [4, 5, 7, 8, 6, 9, 2]
 
 
 def find_averange(numbers):
+   
     averange = sum(numbers) / len(numbers)
+    
     return int(averange)
 
 
 averange1 = find_averange(my_numbers1)
 averange2 = find_averange(my_numbers2)
+
+
+# 2) Пример
+def count_symbols(string: str):
+    
+    symbols = string
+    count = 0
+    for char in symbols:
+        if char == "a":
+            count += 1
+    
+    return count
+
+
+# 3) Пример
+def format_date(day: int, month: str):
+
+    return f"The day {day} in {month} month"
