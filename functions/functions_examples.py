f9213d211ff4272cfb1cb25bbd72aafccb03897b
K0ndrich <123kondrich@gmail.com>
2024-08-15 15:31:14 +0300

    edited

--- functions/functions_examples.py
+++ functions/functions_examples.py
@@ -6,9 +6,9 @@ my_numbers2 = [4, 5, 7, 8, 6, 9, 2]
 
 
 def find_averange(numbers):
-   
+
     averange = sum(numbers) / len(numbers)
-    
+
     return int(averange)
 
 
@@ -18,17 +18,20 @@ averange2 = find_averange(my_numbers2)
 
 # 2) Пример
 def count_symbols(string: str):
-    
+
     symbols = string
     count = 0
     for char in symbols:
         if char == "a":
             count += 1
-    
+
     return count
 
 
 # 3) Пример
-def format_date(day: int, month: str):
+def custom_greeting(*, name: str, greeting: str = "Hello") -> str:
 
-    return f"The day {day} in {month} month"
+    return f"{greeting} -> {name}"
+
+
+a = custom_greeting(name="Max")  # -> Hello -> Max
