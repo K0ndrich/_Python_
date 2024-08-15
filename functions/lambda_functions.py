0c64f8db28f676359760f72b91fd2bdaf794cb2a
K0ndrich <123kondrich@gmail.com>
2024-08-15 11:09:04 +0300

    edited

--- functions/lambda_functions.py
+++ functions/lambda_functions.py
@@ -4,5 +4,9 @@
 # (lambda my_argument1, my_argument2: my_argument1 + my_argument2) (my_value1, my_value2)
 (lambda a, b: a + b)(3, 5)  # -> 8
 
-# добавление действйи в функцию
+# добавление действий в функцию
 (lambda a, b: a if a > b else b)(3, 5)  # -> 5
+
+(lambda a, b: abs(a) if a > b else float(b))(3, 5)  # -> 5.0
+
+
