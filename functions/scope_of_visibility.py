8bb312d2f6957a28d09af8572ef5f41615924505
K0ndrich <123kondrich@gmail.com>
2024-08-17 11:11:28 +0300

    edited

--- functions/scope_of_visibility.py
+++ functions/scope_of_visibility.py
@@ -1,7 +1,7 @@
 # Области Видимости переменных в функциях
 
 
-# 1) Пример
+# -----   1) Пример   -------------------------------------------------------------------------------------------------------------------------------------------------------
 # local_var нельзя вызвать вне области функции
 def my_function():
 
@@ -10,9 +10,10 @@ def my_function():
 
 
 # my_function()
+# local_var  # -> Error
 
 
-# 2) Пример
+# -----   2) Пример   -------------------------------------------------------------------------------------------------------------------------------------------------------
 variable = "1"
 
 
@@ -22,5 +23,53 @@ def my_function():
     print(variable)
 
 
-my_function()  # -> 2
-print(variable)  # -> 1
+# my_function()  # -> 2
+variable  # -> 1
+
+
+# -----   3) Пример   -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+COMFORTABLE_TEMPERATURE = 25
+
+
+def my_function(*, temperature: int):
+    return COMFORTABLE_TEMPERATURE - temperature
+
+
+my_function(temperature=30)  # -> -5
+
+
+# -----   4) Пример   -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+my_var = 1
+
+
+def my_function():
+    # global указывает что будем использовать глобальную переменную
+    global my_var
+    my_var = 2
+
+
+my_var  # -> 1
+my_function()
+my_var  # -> 2
+
+
+# -----   5) Пример   -------------------------------------------------------------------------------------------------------------------------------------------------------
+DEFAULT_LEVEL_EXPERIENCE = 200
+
+
+# функция определяет нужно ли повишать уровень персонажа в игре
+def is_level_up(*, current_experience: int, ganied_experience: int):
+    total_experience = current_experience + ganied_experience
+    level_up = False
+
+    if total_experience >= DEFAULT_LEVEL_EXPERIENCE:
+        # level_up = True повишаем левер пресонажа
+        level_up = True
+
+    return level_up
+
+
+is_level_up(current_experience=150, ganied_experience=30)  # -> False
+is_level_up(current_experience=150, ganied_experience=70)  # -> True
