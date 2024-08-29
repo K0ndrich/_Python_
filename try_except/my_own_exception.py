a76fdce6276a3bf5dd626c3f380d8ae95acaeaf7
K0ndrich <123kondrich@gmail.com>
2024-08-29 18:23:49 +0300

    edited

---
+++ try_except/my_own_exception.py
@@ -0,0 +1,13 @@
+# Создание Своего Исключения (Ошибки)
+
+# Нужен для получения больше информации при появлении нашей ошибки(исключения)
+
+
+class MyException(Exception):
+    def __init__(self, value, message="My Text Message"):
+        self.value = value
+        self.message = message
+        super().__init__(self.message)
+
+
+raise MyException("text")  # -> MyException: My Text Message
--- try_except/try_excepts.py
+++ try_except/try_excepts.py
@@ -3,16 +3,40 @@
 
 # try ожидает ошибку в своем блоке кода
 try:
-    pass
+    print("try")
 
 
 # except при словленном try ошибке будет выполнять код внутри себя
 # обрабатывает все ошибки
-# except:
-#     pass
+except Exception:
+    pass
+
+
+# обрабатываеть только указанные несколько типов ошибки 
+except (ValueError , NameError):
+    pass
+
+
+# обрабатывает все ошибки
+# второй(текущий) exсept выполянеться если возникла ошибка в первом except
+# try -> except Exception -> except(current)
+except:
+    pass
+
+
+# else выполняеться, если полностью выполнился try без ошибок
+# try -> else
+else:
+    pass
+
+
+# finally выполняеться в любом случае в конце выполнение верхних блоков кода
+# try -> else -> finally
+finally:
+    pass
 
 
 # можно также указывать только один тип ошибки, который будем обрабатывать
 # as my_name позволяет обращаться к ошибке внутри кода
-except ZeroDivisionError as error:
-    pass
+# except ZeroDivisionError as error:
+#     pass
