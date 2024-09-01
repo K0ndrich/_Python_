9df9fad76213f0dc60e823f228207db9b3e0d593
K0ndrich <123kondrich@gmail.com>
2024-09-01 20:53:23 +0300

    edited

--- OOP/property.py
+++ OOP/property.py
@@ -12,38 +12,63 @@ class MyClass:
         # __a нельзя обращаться к таким отрибутам через обьект
         self.__a = a
 
-    def get_a(self):
+    # def get_a(self):
+    #     return self.__a
+
+    # def set_a(self, a):
+
+    #     self.__a = a
+    # def del_a(self):
+    #     pass
+
+    # 1) ВАРИАНТ
+    # нужен чтобы писать my_object.a вместо my_object.get_a()
+    # теперь а становить обьектов property, а не просто переменной
+    # a = property(get_a, set_a , del_a)
+
+    # 2) ВАРИАНТ
+    # a = property()
+    # a = a.getter(get_a)
+    # a = a.setter(set_a)
+    # a = a.deleter(del_a)
+
+    # 3) ВАРИАНТ
+    @property
+    def a(self):
         return self.__a
 
-    def set_a(self, a):
+    @a.setter
+    def a(self, a):
         self.__a = a
 
-    # нужен чтобы писать my_object.a вместо my_object.get_a()
-    a = property(get_a, set_a)
+    @a.deleter
+    def a(self):
+        del self.__a
+        # print("Delete was successful")
 
 
 my_object = MyClass(7)
 
-
+# геттер
 my_object.a  # -> 7
 
-
-my_object.a = 10
-my_object.a  # -> 10
-
-
-my_object.__dict__  # -> {'_MyClass1__a': 10}
+# сеттер
+my_object.a = 15
+my_object.a  # -> 15
 
 
-my_object.get_a()  # -> 10
+my_object.__dict__  # -> {'_MyClass__a': 15}
 
-my_object.set_a(15)
-my_object.get_a()  # -> 15
+# делитер
+del my_object.a  # -> Delete was successful
+
+my_object.__dict__  # -> {}
 
 
 # самостоятельно создаеться новый атрибут для обьекта класса
+# етот атрибут b локальный типа public
 my_object.b = 777
 my_object.b  # -> 777
+my_object.a = 15
 
-# b являеться локальным(public) атрибутом обьекта класса
-my_object.__dict__  # -> {'_MyClass__a': 15, 'b': 777}
+my_object.__dict__  # -> {'b': 777, '_MyClass__a': 15}
