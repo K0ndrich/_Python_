df4f1ebb23b23c8d6a591950bcd4032de1d962b4
K0ndrich <123kondrich@gmail.com>
2024-09-17 13:53:20 +0300

    edited

---
+++ OOP/multiple_inheritance.py
@@ -0,0 +1,48 @@
+# Множественное Наследование
+# Миксин (Mixin) -  это классы у которых нет данных(атрибутов), но есть методы. Основная ихняя функция добавить методов классу, но при етом его не изменять
+
+# В пример берем технику, которая продаеться в нашем магазине
+
+
+class Item:
+
+    # создает один товар нашего магазина
+    def __init__(self, name, weight, price):
+        print("- Create Item -")
+        super().__init__()
+        self.name = name
+        self.weight = weight
+        self.price = price
+
+    # выводит информацию о нашем товаре
+    def print_info(self):
+        print(f"{self.name}, {self.weight} , {self.price}")
+
+
+class Mixin:
+
+    # просто хранит id каждого товара в магазине
+    ID = 0
+
+    # создание id для товара
+    # етот init для NoteBook не выполниться, если опередел первый init в Item
+    def __init__(self):
+        print("- Create Mixin -")
+        self.ID += 1
+        self.id = self.ID
+
+    def save_sell_log(self):
+        print(f"{self.id}: product was sold at 00:00 hours")
+
+
+# дочерний класс, который описывает ноутбуки
+# множественное наследование происходит слева на право, сначала наследуються атрибуты и методы Item, а потом уже от Mixin
+class NoteBook(Item, Mixin):
+    pass
+
+
+my_notebook1 = NoteBook("Asus", 1.5, 3500)
+my_notebook2 = NoteBook("Asus", 1.5, 3500)
+my_notebook3 = NoteBook("Asus", 1.5, 3500)
+# my_notebook.print_info()  # -> Asus, 1.5 , 3500
+my_notebook3.save_sell_log()
