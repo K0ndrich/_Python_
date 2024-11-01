33508f4d9909316b8c47ea8b07f5e10ff22c7f98
K0ndrich <123kondrich@gmail.com>
2024-11-01 19:18:41 +0200

    asdas

---
+++ test.py
@@ -0,0 +1,18 @@
+class Document:
+    pass
+
+class SCVDocuments
+        
+
+
+class B:
+    def say_hi(self):
+        print("Hi from B")
+
+
+def some(my_object):
+    my_object.say_hi()
+
+
+my_a = B()
+some(my_a)
